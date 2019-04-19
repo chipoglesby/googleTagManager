66b6dfc65ffe71e155dba907281a41b3ec807c94
Chip Oglesby <chip@creativesparkcolumbia.com>
2019-04-19 00:12:38 -0400

    Update findGCLID.js

--- userDefinedVariables/findGCLID.js
+++ userDefinedVariables/findGCLID.js
@@ -1,3 +1,9 @@
 function() {
-  return window._dataLayerHistory.history.filter(function(object) { return typeof object.gclid === 'string'; }).valueOf()[0].gclid;
+  var times = window._dataLayerHistory.history.filter(function(obj) { return typeof obj.fbclid === 'string'; }).length;
+if (times > 0) {
+  times = times - 1;
+} else {
+  times = 0
+}
+  return window._dataLayerHistory.history.filter(function(object) { return typeof object.fbclid === 'string'; }).valueOf()[times].fbclid;
 }
