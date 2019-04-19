339797ecf297b8688be9a814eec6a6a9e9508540
Chip Oglesby <chip@creativesparkcolumbia.com>
2019-04-19 00:26:46 -0400

    Update findGCLID.js
    Copied and pasted the wrong code. Correcting.

--- userDefinedVariables/findGCLID.js
+++ userDefinedVariables/findGCLID.js
@@ -1,9 +1,9 @@
 function() {
-  var times = window._dataLayerHistory.history.filter(function(obj) { return typeof obj.fbclid === 'string'; }).length;
+  var times = window._dataLayerHistory.history.filter(function(obj) { return typeof obj.gclid === 'string'; }).length;
 if (times > 0) {
   times = times - 1;
 } else {
   times = 0
 }
-  return window._dataLayerHistory.history.filter(function(object) { return typeof object.fbclid === 'string'; }).valueOf()[times].fbclid;
+  return window._dataLayerHistory.history.filter(function(object) { return typeof object.gclid === 'string'; }).valueOf()[times].gclid;
 }
